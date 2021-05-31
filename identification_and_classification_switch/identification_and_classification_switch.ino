0a844b35663f29ae1b57f25adeb50c3770b43e52
Jardel <jardel.dyonisio@hotmail.com>
2021-05-31 03:29:13 -0300

    Testing with vector

--- identification_and_classification_switch/identification_and_classification_switch.ino
+++ identification_and_classification_switch/identification_and_classification_switch.ino
@@ -1,9 +1,11 @@
-int tval = 0, average = 0, cont = 0, state = 0, value, limit, contt = 0;
+int tval = 0, average = 0, cont = 0, state = 0, value, limit, contt = 0, i = 0;
 
 unsigned long timee;
 unsigned long timeinit;
 unsigned long timend;
 unsigned long ttotal;
+
+int vec[500];
 
 /*
 states:
@@ -28,7 +30,7 @@ void loop()
   {
     case 0:
       digitalWrite(7, HIGH);
-      limit = analogRead(A0) + 70;
+      limit = analogRead(A0) + 50;
       state++;
       break; 
     case 1:
@@ -37,28 +39,69 @@ void loop()
         value = analogRead(A0);
         timee = millis();
 
-        Serial.println(value);
+        //Serial.println(value);
         
         if (value >= limit)
         {
-          Serial.println("Open limit if");
+          //Serial.println("Open limit if");
+          
           if(contt == 0)
           {
             timeinit = timee;
             tval = 0;
             contt++; 
           }
+
+          vec[i] = value;
+
           tval = tval + value;
           cont++;
+          i++;
           
-          Serial.print("Tval: ");
-          Serial.println(tval);
+          /*Serial.print("Tval: ");
+          Serial.println(tval);*/
         }
   
         if(contt != 0)
         { 
           timend = timee;
           ttotal = timend - timeinit;
+
+          /*Serial.print("Inicial time: ");
+          Serial.println(timeinit);
+
+          Serial.print("Final time: ");
+          Serial.println(timend);
+
+          Serial.print("Ttotal: ");
+          Serial.println(ttotal);
+
+          Serial.print("ttaol: ");
+          Serial.println(ttotal);*/
+  
+          if(ttotal >= 200)
+          {
+            state++;
+            break;
+          }
+        }
+      }
+    case 2:
+      average = tval/cont;
+
+      /*Serial.print("State: ");
+      Serial.println(state);*/
+  
+      if (state == 2 && value < limit)
+      {
+        if(average > 300)
+        {
+          Serial.println("Semente de arroz vermelho");
+          Serial.print("average: ");
+          Serial.println(average);
+
+          Serial.print("Tval: ");
+          Serial.println(tval);
 
           Serial.print("Inicial time: ");
           Serial.println(timeinit);
@@ -68,29 +111,16 @@ void loop()
 
           Serial.print("Ttotal: ");
           Serial.println(ttotal);
-  
-          if(ttotal >= 1000)
-          {
-            Serial.print("timee: ");
-            Serial.println(timee);
-            state++;
-            break;
-          }
-        }
-      }
-    case 2:
-      average = tval/cont;
 
-      Serial.print("State: ");
-      Serial.println(state);
-  
-      if (state == 2 && value < limit)
-      {
-        if(average > 300)
-        {
-          Serial.println("Semente de arroz vermelho");
-          Serial.print("average: ");
-          Serial.println(average);
+          Serial.print("cont: ");
+          Serial.println(cont);
+
+          for (int i = 0; i < cont; i++)
+          {
+            Serial.print(vec[i]);
+            Serial.print(" "); 
+          }
+          
           break;
         }
         else
@@ -98,6 +128,28 @@ void loop()
           Serial.println("Semente de arroz branco");
           Serial.print("average: ");
           Serial.println(average);
+
+          Serial.print("Tval: ");
+          Serial.println(tval);
+
+          Serial.print("Inicial time: ");
+          Serial.println(timeinit);
+
+          Serial.print("Final time: ");
+          Serial.println(timend);
+
+          Serial.print("Ttotal: ");
+          Serial.println(ttotal);
+
+          Serial.print("cont: ");
+          Serial.println(cont);
+
+          for (int i = 0; i < cont; i++)
+          {
+            Serial.print(vec[i]);
+            Serial.print(" "); 
+          }
+          
           break;
         }
       }
