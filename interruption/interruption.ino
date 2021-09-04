aaa5d7520a1df56dc4c87154066016f837c7fb61
Jardel <jardel.dyonisio@hotmail.com>
2021-09-03 21:53:41 -0300

    Vector problem

--- interruption/interruption.ino
+++ interruption/interruption.ino
@@ -1,12 +1,17 @@
 #define LED_PIN 7
+#define SENSOR_PIN A0
+#define pinSensor
 
+int valueSave[500];
+int valueMonitor, limit, countl = 0, tval = 0, average, activator = 0;
+int cont = 0;
 int estado_led = 0;
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(7, OUTPUT);
-  digitalWrite(7, HIGH);   
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);   
   
   // Configuração do timer1 //Liga interrupção
   TCCR1A = 0;                        //confira timer para operação normal pinos OC1A e OC1B desconectados
@@ -15,80 +20,68 @@ void setup()
   TIMSK1 |= (1 << TOIE1);           // habilita a interrupção do TIMER1
 } 
 
+void loop()
+{
+  if (cont < 500)
+  {
+    for (int i = 0; i < 499; i ++)
+    {
+      tval = tval + valueSave[i];
+      
+      Serial.print("Value: ");
+      Serial.println(valueSave[i]);
+    }
+  }
+
+  if ((cont = 500) && (activator = 1))
+  {
+    average = tval/cont;
+    Serial.print("Average: ");
+    Serial.println(average);
+
+    Serial.println("Encheu o vetor");
+
+    cont = 0;
+    tval = 0;
+    activator = 0;
+  }
+}
+
 ISR(TIMER1_OVF_vect)//interrupção do TIMER1 com frequencia de 1Hz //Função da biblioteca arduino
 {
   TCNT1 += 49536; //Renicia TIMER //65536 - 16000 = 49536 (valor a ser carregado no registrador de 16 bits)
 
-  if (estado_led >= 1000)
+  if (countl == 0)
   {
-    estado_led = 0;
+    limit = valueMonitor + 50; 
+    countl++;
+  }
+
+  if(cont < 500)
+  {
+    valueMonitor = analogRead(SENSOR_PIN);
+    
+      if (valueMonitor > limit)
+      {
+        valueSave[cont] = analogRead(SENSOR_PIN);
+        activator = 1;
+        cont++;
+      }
+  }
+
+  /*if (cont >= 1000)
+  {
+    cont = 0;
   }
   
-  estado_led++;
+  cont++;
 
-  if (estado_led >= 500)
+  if (cont >= 500)
   {
     digitalWrite(LED_PIN, HIGH);
   }
   else
   {
     digitalWrite(LED_PIN, LOW);
-  }
-
-  /*if(tx_fsm >= TX_START)
-  {
-    if(tx_fsm == TX_START)
-    {
-       tx_tick_count = 3; // na próxima interrupção, inicie o start bit.
-       tx_fsm++;
-       tx_shift_frame = ((unsigned int)tx_byte<<1)|0xFE00; // 7 bits em 1 seguido do byte a ser transmitido seguido de zero (frame completo em uma palavra)
-    }
-    else // vamos transmitir o frame de dados (start byte stop)
-    {
-      tx_tick_count++;
-      
-      if((tx_tick_count & 3)== 0) // forma eficiente de testar se passou 4 unidades
-      { 
-        digitalWrite(TX_PIN, tx_shift_frame & 1);
-        tx_shift_frame >>=1;
-        tx_fsm++;
-        
-        if(tx_fsm>TX_STOP_BIT)
-        {
-          tx_fsm=TX_IDLE; // transmitiu todo o frame, não precisa fazer mais nada.        
-        }
-      }  
-    }
-  }
-
-  if(rx_fsm == RX_IDLE)
-  {
-    if(digitalRead(RX_PIN) == 0) // start bit detectado
-    {
-      rx_tick_count=3;
-      rx_fsm++;      
-    }
-  }
-  else // tem byte sendo recebido
-  {
-    rx_tick_count++;
-    
-    if((rx_tick_count & 3)== 0) // forma eficiente de testar se passou 4 unidades
-    {
-      rx_fsm++;
-      
-      if(rx_fsm <= RX_STOP_BIT)
-      {
-        rx_shift>>=1;   
-        
-        if(digitalRead(RX_PIN))rx_shift+=0x80; //0x80 = 0000 0001 0000 0000
-      }
-      else
-      {
-        rx_fsm=RX_IDLE;
-        rx_byte=rx_shift;
-        rx_new_byte=1; // notifica que há um novo byte recebido.
-      }
-    }
   }*/
 }
